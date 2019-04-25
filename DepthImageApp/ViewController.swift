9216364ff7da5135ac0364a27c67d4d46df01fd6
halsey <wsh940808@163.com>
2019-04-25 22:32:11 +0800

    更新了pods；增加了主页的界面

--- DepthImageApp/ViewController.swift
+++ DepthImageApp/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    var iv: UIImageView?
+    var btnSelectPhoto: UIButton?
+    let screenWidth = UIScreen.main.bounds.width
+    
+    override func loadView() {
+        super.loadView()
+        initView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController {
+    func initView() {
+        iv = UIImageView.init()
+        iv?.backgroundColor = .lightGray
+        self.view.addSubview(iv!)
+        iv?.snp.makeConstraints({ (make) in
+            make.center.width.equalToSuperview()
+            make.height.equalTo(screenWidth)
+        })
+        
+        btnSelectPhoto = UIButton.init()
+        btnSelectPhoto?.setTitle("Select", for: .normal)
+        btnSelectPhoto?.backgroundColor = .darkGray
+        self.view.addSubview(btnSelectPhoto!)
+        btnSelectPhoto?.snp.makeConstraints({ (make) in
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(iv!.snp.bottom).offset(30)
+        })
+    }
+}
